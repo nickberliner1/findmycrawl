1eb19f68d71419edc6ad1b67b774318c88c086a3
Nick Berliner <nick.berliner96@gmail.com>
2021-01-31 11:35:53 +0100

    backend added

---
+++ findmycrawl-frontend/components/Search.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <input
+            class="search"
+            placeholder="Search your city"
+            :search="search"
+        />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Search',
+    components: {
+        
+    },
+
+	methods: {
+		search(input) {
+			if (input.length < 1) { 
+                return [] 
+            }
+			return cities.filter(city => {
+                return city.toLowerCase()
+                .startsWith(input.toLowerCase())
+			})
+		}
+  	}
+    
+}
+</script>
+
+<style scoped>
+
+.search {
+    border-radius: 10px;
+    width: 40vw;
+    height: 7vh;
+}
+
+</style>
---
+++ findmycrawl-frontend/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="homepage">
+		<div class="hero-container">
+			<div class="hero-content">
+				<h1 class="title">
+				Find Your Crawl
+				</h1>
+				<Search />
+			</div>		
+			
+		</div>
+	</div>
+</template>
+
+<script>
+import Search from '../components/Search';
+
+export default {
+	components: {
+		Search
+	}
+}
+</script>
+
+<style>
+
+.hero-container {
+	background-image: 
+		linear-gradient(
+			rgba(17,17,17,.35),
+			rgba(17,17,17,.35)
+		),
+		url("https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80");
+	background-size: cover;
+	min-height: 100vh;
+	box-sizing: border-box;
+	margin: 0;
+	display: flex;
+	justify-content: center;
+}
+
+.hero-content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: white;
+  letter-spacing: 1px;
+}
+
+</style>
