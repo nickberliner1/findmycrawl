dc4b9e313c0c2fb1f99edac3217c5db297c0f29d
Nick Berliner <nick.berliner96@gmail.com>
2021-02-11 15:03:23 +0100

    api working

--- fmcbackend/api/models.py
+++ fmcbackend/api/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Event(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length = 60)
+
+    # objects = QuerySetManager()
 
     def __str__(self):
         return self.name
--- fmcbackend/api/views.py
+++ fmcbackend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .serializers import EventSerializer
+from .models import Event
+
 # Create your views here.
+
+class EventViewSet(viewsets.ModelViewSet):
+    '''I have to write this because VSC won't shut up'''
+    queryset = Event.objects.all().order_by('name')
+    serializer_class = EventSerializer
