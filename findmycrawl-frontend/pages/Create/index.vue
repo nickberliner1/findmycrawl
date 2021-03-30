d5de463b168529188af8b25d446a3aa22595edb6
Nick Berliner <nick.berliner96@gmail.com>
2021-03-30 10:16:49 +0200

    POST request aborted

--- findmycrawl-frontend/pages/Create/index.vue
+++ findmycrawl-frontend/pages/Create/index.vue
@@ -1,11 +1,42 @@
 <template>
     <div class="container">
         <h2>Create event</h2>
+        <form @submit="createEvent">
+            <strong>Name: </strong>
+            <input type="text" v-model="name" placeholder="Name" />
+            <strong>Price: </strong>
+            <input type="text" v-model="ticket_price" placeholder="Price" />
+            <button class="btn">Submit</button>
+        </form>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+
+    data() {
+        return {
+            name: '',
+            ticket_price: ''
+        }
+    },
+
+    methods: {
+        async createEvent() {
+            await axios.post(`http://localhost:8000/events`, {
+                name: this.name,
+                ticket_price: this.ticket_price
+            })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }
     
 }
 </script>
--- findmycrawl-frontend/pages/Events/index.vue
+++ findmycrawl-frontend/pages/Events/index.vue
@@ -59,7 +59,6 @@ export default {
         } catch (err) {
             console.log(err)
         }
-        
     },
 }
 
