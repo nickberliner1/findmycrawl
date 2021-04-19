cc7d9babeb6df566cb6dc109ee2936034e881009
Nick Berliner <nick.berliner96@gmail.com>
2021-04-19 17:03:49 +0100

    indenting

--- findmycrawl-frontend/pages/Create/index.vue
+++ findmycrawl-frontend/pages/Create/index.vue
@@ -3,11 +3,11 @@
         <h2>Create event</h2>
         <form @submit="createEvent">
             <strong>Name: </strong>
-            <input type="text" v-model="name" placeholder="Name" />
+            <input type="text" v-model="event.name" placeholder="Name" />
             <strong>Description: </strong>
-            <input type="textarea" v-model="description" placeholder="Describe your event" />
+            <input type="textarea" v-model="event.description" placeholder="Describe your event" />
             <strong>Price: </strong>
-            <input type="text" v-model="ticket_price" placeholder="Price" />
+            <input type="text" v-model="event.ticket_price" placeholder="Price" />
             <button class="btn" type="submit">Submit</button>
         </form>
     </div>
@@ -20,31 +20,29 @@ export default {
 
     data() {
         return {
-            name: '',
-            description: '',
-            ticket_price: ''
+            event: {
+                name: '',
+                description: '',
+                ticket_price: null
+            }
         }
     },
 
     methods: {
-        createEvent() {
-            const data = {
-                name: '',
-                description: '',
-                ticket_price: ''
-            };
-
-            axios.post(`http://localhost:8000/events/?format=api`, data, {
+        async createEvent() {
+            const config = {
                 headers: {
-                    'Content-type': 'application/json'
+                    'content-type': 'multipart/form-data'
                 }
-            })
-            .then(res => {
-                console.log(res)
-            })
-            .catch(err => {
-                console.log(err.res)
-            })
+            };
+            
+            let formData = new FormData();
+
+            try {
+                let response = await this.$axios.$post('http://localhost:8000/events/', formData, config);
+            } catch (e) {
+                console.log(e);
+            }
         }
     }
     
