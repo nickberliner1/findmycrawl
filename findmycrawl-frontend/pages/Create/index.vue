ae05c6322a8e81658e7ae2d3a34722fdccd10c29
Nick Berliner <40582488+nickberliner1@users.noreply.github.com>
2021-06-15 22:09:36 +0200

    axios functioning (backend still not receiving data though)

--- findmycrawl-frontend/pages/Create/index.vue
+++ findmycrawl-frontend/pages/Create/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container">
         <h2>Create event</h2>
-        <form @submit="createEvent">
+        <form @submit.prevent="createEvent">
             <strong>Name: </strong>
-            <input type="text" v-model="event.name" placeholder="Name" />
+            <input type="text" v-model="name" placeholder="Name" />
             <strong>Description: </strong>
-            <input type="textarea" v-model="event.description" placeholder="Describe your event" />
+            <input type="textarea" v-model="description" placeholder="Describe your event" />
             <strong>Price: </strong>
-            <input type="text" v-model="event.ticket_price" placeholder="Price" />
+            <input type="text" v-model="ticket_price" placeholder="Price" />
             <button class="btn" type="submit">Submit</button>
         </form>
     </div>
@@ -18,39 +18,24 @@ import axios from 'axios';
 
 export default {
 
-    data() {
-        return {
-            event: {
-                name: '',
-                description: '',
-                ticket_price: null
-            }
-        }
-    },
+	data() {
+		return {
+			name: '',
+			description: '',
+			ticket_price: null
+		}
+	},
 
     methods: {
-        async createEvent() {
-            // const config = {
-            //     headers: {
-            //         'content-type': 'multipart/form-data'
-            //     }
-            // };
-            
-            // let formData = new FormData();
-
-            // try {
-            //     let response = await this.$axios.$post('http://localhost:8000/events/', formData, config);
-            // } catch (e) {
-            //     console.log(e);
-            // }
-            axios.post('http://localhost:8000/events', 
-                {
-                    name: 'test bar crawl',
-                    description: 'this is a test bar crawl',
-                    ticket_price: 20
-                }
-            )
-        }
+			createEvent() {
+				this.$axios.post('http://localhost:8000/events', 
+					{
+							name: this.name,
+							description: this.description,
+							ticket_price: this.ticket_price
+					}
+				)
+			}
     }
     
 }
